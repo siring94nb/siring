67d118400b84bd44b6a797906af62cee8945f97f
lilu1233 <lilusiring@163.com>
2019-10-31 14:55:11 +0800

    pull

--- admin/application/admin/controller/Goods.php
+++ admin/application/admin/controller/Goods.php
@@ -301,14 +301,13 @@ class Goods extends Base{
         $size = $this->request->get('size', config('apiAdmin.ADMIN_LIST_DEFAULT'));
         $page = $this->request->get('page', 1);
         $project_name = $this->request->get('project_name', '');
-        $where['del_time']='null';
+        $where['del_time']=NULL;
         if ($project_name) {
             $where['project_name'] = ['like', "%{$goods_name}%"];
         }
-        $list=Db::table('made_good')->where($where)->order('id desc')->paginate($size, false, ['page' => $page]);
-        $listInfo = $list;
+        $list=Db::table('made_good')->where($where)->order('id desc')->paginate($size, false, ['page' => $page])->toArray();
         return $this->buildSuccess([
-            'list'  => $listInfo,
+            'list'  => $list,
         ]);
     }
 
