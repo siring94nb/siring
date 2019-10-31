a28167e10c11976f7808da415b8094c984e70e37
lilu1233 <lilusiring@163.com>
2019-10-31 15:16:20 +0800

    pull

--- admin/application/admin/controller/Goods.php
+++ admin/application/admin/controller/Goods.php
@@ -306,8 +306,8 @@ class Goods extends Base{
             $where['project_name'] = ['like', "%{$goods_name}%"];
         }
         $list=Db::table('made_good')->where($where)->order('id desc')->paginate($size, false, ['page' => $page])->toArray();
-        foreach($list as $k =>$v){
-            $list[$k]['develop']=json_decode($v['develop']);
+        foreach($list['data'] as $k =>$v){
+            $list['data'][$k]['develop']=json_decode($v['develop'],true);
         }
         $listcount=Db::table('made_good')->where($where)->count();
         return $this->buildSuccess([
