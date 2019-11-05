f88af9aaa77b83974153d36c05451525d52eb0df
DESKTOP-M2L3DN7\PHP <414734650@qq.com>
2019-11-05 15:29:52 +0800

    优化

--- web/build/config.js
+++ web/build/config.js
@@ -1,8 +1,8 @@
 let config = {
     env: 'development',
-    baseUrl: 'https://manage.siring.com.cn/admin',
-    front_url: 'https://manage.siring.com.cn/api/'
-    // baseUrl: 'http://localhost/siring/admin/public/index.php/admin',
-    // front_url: 'http://localhost/siring/admin/public/index.php/api/'
+    // baseUrl: 'https://manage.siring.com.cn/admin',
+    // front_url: 'https://manage.siring.com.cn/api/'
+    baseUrl: 'http://localhost/siring/admin/public/index.php/admin',
+    front_url: 'http://localhost/siring/admin/public/index.php/api/'
 };
 export default config;
