61a4b9167d1df260e6fe92db1a429544100be369
lilu1233 <lilusiring@163.com>
2019-11-06 18:15:47 +0800

    pull

--- admin/application/api/controller/AppletManage.php
+++ admin/application/api/controller/AppletManage.php
@@ -22,14 +22,38 @@ class AppletManage extends Base {
     public function get_model_list()
     {
         $request=Request::instance();
+        $postAData=$request->param();
         $model_list=Templete::all(function($query){
-            $query->where('model_status',1)->field('id,model_name,model_image,model_type');
+            $where['model_status']=0;
+            $where['model_type']=$postAData['model_type'];
+            $query->where($where)->field('id,model_name,model_image,model_type');
         })->toArray();
         if($model_list){
             return json(['code'=>1,'msg'=>'获取列表成功','data'=>$model_list]);
         }else{
             return json(['code'=>0,'msg'=>'获取失败']);
         }
+    }
+
+    /**
+     * lilu
+     * 获取模板套餐
+     * param  model_meal_category   1   diy    2 固定
+     */
+    public function get_model_meal_by_category()
+    {
+        $request=Request::instance();
+        $postAData=$request->param();
+        $model_list=Templete::all(function($query){
+            $where['model_meal_category']=$postAData['model_meal_category'];
+            $query->where($where)->field('id,model_name,model_image,model_type')->order('model_rank');
+        })->toArray();
+        if($model_list){
+            return json(['code'=>1,'msg'=>'获取模板套餐成功','data'=>$model_list]);
+        }else{
+            return json(['code'=>0,'msg'=>'获取模板套餐失败']);
+        }
+
     }
 
 
