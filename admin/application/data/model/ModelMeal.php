50a39ef97d21fb51bb8c60007c9edc42bd2bd58b
lilu1233 <lilusiring@163.com>
2019-11-08 17:09:41 +0800

    pull

---
+++ admin/application/data/model/ModelMeal.php
@@ -0,0 +1,30 @@
+<?php
+namespace app\data\model;
+use think\Model;
+
+/**
+ * lilu
+ * 行业模板套餐
+ */
+class ModelMeal extends Model
+{
+    /**
+     * 用户model
+     * @var string
+     */
+    protected $table = "model_meal";
+    protected $resultSetType = 'collection';
+
+    /**
+     * lilu
+     * 获取分类模板套餐list
+     * model_meal_category     1  diy   2 固定样式
+     */
+    public function get_model_meal($model_meal_category)
+    {
+        return self::all(['model_meal_category'=>$model_meal_category,'del_time'=>null])->toArray();
+    }
+
+
+
+}
