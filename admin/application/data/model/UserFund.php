591285b5af97493e765ad4663ac4885675b78f23
DESKTOP-M2L3DN7\PHP <414734650@qq.com>
2019-11-12 18:38:05 +0800

    控制台

--- admin/application/data/model/UserFund.php
+++ admin/application/data/model/UserFund.php
@@ -13,4 +13,10 @@ class UserFund extends Model
     protected $table = "user_fund";
     protected $resultSetType = 'collection';
 
+    //用户个人余额信息
+    public static function user($uid)
+    {
+        return self::get(['user_id'=>$uid]) ? self::get(['user_id'=>$uid])->toArray() : returnJson(0,'数据有误');
+    }
+
 }
