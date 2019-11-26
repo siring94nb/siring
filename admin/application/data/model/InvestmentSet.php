fa4d1254119a270824dda8c6a1b3e3d75f10fec8
DESKTOP-M2L3DN7\PHP <414734650@qq.com>
2019-11-26 16:37:32 +0800

    投融界

--- admin/application/data/model/InvestmentSet.php
+++ admin/application/data/model/InvestmentSet.php
@@ -16,4 +16,36 @@ class InvestmentSet extends Model
     protected $autoWriteTimestamp = true;
     protected $createTime = 'created_at';
     protected $updateTime = 'updated_at';
+
+    /**
+     * 分类费用
+     * @return array
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\ModelNotFoundException
+     * @throws \think\exception\DbException
+     */
+    public static function industry_index()
+    {
+        $field = 'id,cost,reward,cid';
+
+        $list = InvestmentSet::with('InvestmentClass') -> field( $field ) ->select()->toArray();
+
+            foreach($list as $key=>$value){
+                $list[$key]['title'] = $value['investment_class']['title'];
+                unset($list[$key]['investment_class']);
+            }
+
+        return $list;
+    }
+
+
+    /**
+     * 关联分类表
+     * @return \think\model\relation\BelongsTo
+     */
+    public function InvestmentClass()
+    {
+        return $this->belongsTo('InvestmentClass', 'cid', 'id');
+    }
+
 }
