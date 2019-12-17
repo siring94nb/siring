5caf5fa5250630caf67e5e8b12699a4bfec4460a
Zheng-jp <970663468@qq.com>
2019-12-17 18:23:38 +0800

    pull

--- frontend/src/views/afterLoggin/demand_order/order_detail.vue
+++ frontend/src/views/afterLoggin/demand_order/order_detail.vue
@@ -170,6 +170,82 @@
             <div>请耐心等待平台报价！</div>
           </div>
         </div>
+        <div class="contract" v-if="status == 3">
+          <p class="serial">合同编号：SR20190202</p>
+          <p class="contract-title">软件/定制开发合同</p>
+          <div class="identity">
+            甲方：
+            <input type="text" placeholder="请选择" style="width:390px;" />
+            <div>选择企业身份</div>
+          </div>
+          <p>乙方：深圳市思锐信息技术有限公司 914403001924280274</p>
+          <p>
+            甲乙双方基于平等互利、公平自愿原则，委托甲方进行
+            <span class="focus">装修小程序开发</span> 项目开发。经友好协商一致同意签订如下合同，共同遵守，双方对本合同的所有内容均无任何异议。
+          </p>
+          <p>
+            一、产品名称：
+            <span class="focus">装修小程序开发</span>
+          </p>
+          <p>
+            二、终端版本:
+            <span class="focus">安卓+苹果+小程序+微信公众号</span>
+          </p>
+          <p>
+            三、项目工期：
+            <span class="focus">20个工作。</span>
+          </p>
+          <p>
+            四、合同总金额：
+            <span class="focus">{{price}}元（{{convertToChinaNum(price)}}元整）</span>
+          </p>
+          <p>五、分期付款方式：甲方向乙方支付总开发费用实行分期付款方式。本项目分四期付款，第一期为总合同款的70%，第二期为总合同款的20%，第三期为总合同款10%,第四期为总合同款10%。</p>
+          <p class="retract">
+            5.1在本合同签订后的3工作日内，第一期甲方支付乙方项目预付款即合同总价的70%，小写：¥__
+            <span class="focus">{{price*0.7}}</span>__（大写：人民币：__
+            <span class="focus">{{convertToChinaNum(price*0.7)}}</span>__元整）。
+          </p>
+          <p class="retract">
+            5.2甲方在和乙方对完整套项目原型及UI时，经甲方签署原型确认单后，3个工作日内需向乙方支付第二期费用即合同总价的10%，小写：¥__
+            <span class="focus">{{price*0.1}}</span>（大写：人民币：__
+            <span class="focus">{{convertToChinaNum(price*0.1)}}</span>__元整）。
+          </p>
+          <p class="retract">
+            5.3乙方完成项目DEMO版：即可运行90%功能的版本（不排除存在BUG），经甲方确认版本功能无误后，3个工作日内需向乙方支付第三期费用即合同总价的10%，小写：¥__
+            <span class="focus">{{price*0.1}}</span>__（大写：人民币：__
+            <span class="focus">{{convertToChinaNum(price*0.1)}}</span>__元整）。
+          </p>
+          <p class="retract">
+            5.4甲方应于收到乙方交付的最终定稿的产品安装包，之日起3个工作日内向乙方支付本合同第四期费用即合同总价的10%，小写：¥__
+            <span class="focus">{{price*0.1}}</span>__（大写：人民币：__
+            <span class="focus">{{convertToChinaNum(price*0.1)}}</span>__元整）。
+          </p>
+          <p>六、项目全自动流程：整体项目开发通过线上自动流程确认完成，并系统化管理方式，每个环节均可在我的软件/定制板块订单中跟进完成所有交易流程和步骤。</p>
+          <p>七、工期计算方式：甲方确认设计原型并付款之日，为项目开发工期的起始日期，乙方完成项目DEMO版之日，为项目完工日期。</p>
+          <p>八、免费维护期： 12个月，免费维护期内，产品出现的原型确认功能范围内的任何bug，乙方需第一时间免费处理。维护期自甲方进行产品确认验收完毕后开始计时。</p>
+          <p>九、有偿维护期：超过12个月后，乙方提供有偿技术维护，平台维护费为本合同金额+新增功能需求总计的10%；</p>
+          <p>十、新增功能需求： 如果甲方需要对于系统进行非内容性的修改，例如系统功能的添加、修改和删除，或对系统整体风格及页面布局进行调整，其费用根据甲方的具体需求另行商定，该费用不包含在本合同内。</p>
+          <p>十一、验收：项目完成，乙方自检完成后，报请甲方验收，甲方接到申请后应在 7 日内进行验收，验收完成后，甲方应在 5日内为乙方办妥竣工验收及结算资料。若甲方未在指定的期限内完成验收工作，到期后即默认视为验收合格。</p>
+          <p>十二、违约责任：甲方在项目制作过程中需与乙方积极配合，提供有效的修改意见或确认信息，若在项目进行过程中，甲方处于停滞状态，即对乙方提供的工作成果既不给出有效修改意见也不对其进行肯定确认，如果停滞时间超过20个工作日以上，则视作甲方主动解除本合同，甲方无权要求乙方返还已经支付的费用。</p>
+          <p>十三、电子合同：本合同电子档，经过甲方同意，付款后立即生效，合同电子档将长期保存在会员账号平台中。</p>
+          <p>十四、纸质合同：如需纸质合同，可将本合同下载打印一式两份，邮寄给乙方，经乙方盖章后，寄还给甲方一份，甲乙双方各执一份。</p>
+          <p>十五、免责：因不可抗力或者其他意外事件，或使得本合同的履行不可能、不必要或者无意义的，任一方均可以解除本合同。本合同所称不可抗力、意外事件是指不能预见、不能克服并不能避免且对一方当事人造成重大影响的客观事件，包括但不限于自然灾害以及社会事件如战争、动乱、政府行为等，但受不可抗力影响的一方应当及时通知对方并采取合理措施防止损失的扩大。</p>
+          <p>十六、协商：双方当事人对本合同的订立、解释、履行、效力等发生争议的，应友好协商解决；协商不成的，双方同意向乙方所在地有管辖权限的人民法院诉讼解决。</p>
+          <p>十七、附件及补充协议：本合同未及事宜，由双方协商作出补充协议，附件及补充协议与本合同具有同等效力。</p>
+          <div class="sign">
+            <div class="party-sign">
+              <p>甲方（签章）：刘德华</p>
+              <p>法定或签约代表：</p>
+              <p>日期： 2018 年 6 月 19 日</p>
+            </div>
+            <div style="flex:1;"></div>
+            <div class="party-sign" style="margin-right:120px;">
+              <p>乙方（签章）：深圳市思锐信息技术有限公司</p>
+              <p>法定或签约代表：</p>
+              <p>日期： 2018 年 6 月 19 日</p>
+            </div>
+          </div>
+        </div>
       </div>
       <div class="line line-vice">平台顾问信息互动</div>
       <!-- 聊天 -->
@@ -200,25 +276,38 @@
           <button class="lt-btn">发送</button>
         </div>
       </div>
-      <div class="obj-btn" >
+      <div class="obj-btn">
         <div class="btns-all stop-obj" @click="obj_btn(1)">中止，本人放弃该项目</div>
         <div style="flex:1;"></div>
-        <div class="btns-all start-obj" @click="obj_btn(2)" v-if="status == 1" >确定，本人已确认该需求方案</div>
-        <div class="btns-all no-obj" v-if="status == 2" >等待报价单</div>
-        <!-- <div class="btns-all start-obj" @click="obj_btn(2)" v-if="status == 2" >确定，本人已确认该报价方案</div> -->
+        <div class="btns-all start-obj" @click="obj_btn(2)" v-if="status == 1">确定，本人已确认该需求方案</div>
+        <div class="btns-all no-obj" v-if="status == 2">等待报价单</div>
+        <!-- <div class="btns-all start-obj" @click="obj_btn(3)" v-if="status == 2" >确定，本人已确认该报价方案</div> -->
+        <div class="btns-all no-obj" @click="obj_btn(4)" v-if="status == 3">等待支付合同款</div>
       </div>
+      <payment-bar
+        ref="paymentbar"
+        :showPaymentFlag="showPaymentFlag"
+        :price="price"
+        :percent="percent"
+        :pay="pay"
+        :order="1"
+        :scale="scale"
+        @sendNum="getNum"
+        :needCodeDialog="needCodeDialog"
+      />
     </div>
   </div>
 </template>
 <script>
 import logginHeader from "@/components/logginHeader";
 import { needOrderList } from "@/api/api";
+import PaymentBar from "@/components/join/paymentBar";
 export default {
   data() {
     return {
       userId: "",
       path: "ws://127.0.0.1:3000",
-      status: 2,
+      status: 3,
       socket: "",
       statusData: [
         {
@@ -309,11 +398,34 @@ export default {
       ],
       UploadAction: "",
       fileList: [],
-      value: ""
+      value: "",
+      showPaymentFlag: false,
+      needCodeDialog: true, //需要显示扫码弹窗
+      price: 50000,
+      percent: 100,
+      scale: 70,
+      payway: {
+        way: 1
+      },
+      wayRule: {
+        way: [
+          {
+            required: true,
+            message: "请选择支付方式",
+            trigger: "change"
+          }
+        ]
+      },
+      payqrcode: ""
     };
   },
   mounted() {
     this.init();
+  },
+  computed: {
+    total() {
+      return this.price * (this.percent / 100) * (this.scale / 100);
+    }
   },
   methods: {
     init() {
@@ -414,14 +526,71 @@ export default {
           message: "action: " + action
         });
       });
-    }
+    },
+    //数字转汉字
+    convertToChinaNum(num) {
+      var arr1 = new Array(
+        "零",
+        "一",
+        "二",
+        "三",
+        "四",
+        "五",
+        "六",
+        "七",
+        "八",
+        "九"
+      );
+      var arr2 = new Array(
+        "",
+        "十",
+        "百",
+        "千",
+        "万",
+        "十",
+        "百",
+        "千",
+        "亿",
+        "十",
+        "百",
+        "千",
+        "万",
+        "十",
+        "百",
+        "千",
+        "亿"
+      ); //可继续追加更高位转换值
+      if (!num || isNaN(num)) {
+        return "零";
+      }
+      var english = num.toString().split("");
+      var result = "";
+      for (var i = 0; i < english.length; i++) {
+        var des_i = english.length - 1 - i; //倒序排列设值
+        result = arr2[i] + result;
+        var arr1_index = english[des_i];
+        result = arr1[arr1_index] + result;
+      } //将【零千、零百】换成【零】 【十零】换成【十】
+      result = result.replace(/零(千|百|十)/g, "零").replace(/十零/g, "十"); //合并中间多个零为一个零
+      result = result.replace(/零+/g, "零"); //将【零亿】换成【亿】【零万】换成【万】
+      result = result.replace(/零亿/g, "亿").replace(/零万/g, "万"); //将【亿万】换成【亿】
+      result = result.replace(/亿万/g, "亿"); //移除末尾的零
+      result = result.replace(/零+$/, ""); //将【零一十】换成【零十】 //result = result.replace(/零一十/g, '零十');//貌似正规读法是零一十 //将【一十】换成【十】
+      result = result.replace(/^一十/g, "十");
+      return result;
+    },
+    getNum(value) {
+      this.num = value;
+    },
+    pay() {}
   },
   destroyed() {
     // 销毁监听
     this.socket.onclose = this.close;
   },
   components: {
-    logginHeader
+    logginHeader,
+    PaymentBar
   }
 };
 </script>
@@ -508,6 +677,55 @@ export default {
           font-size: 20px;
           color: #c9c9c9;
           text-align: center;
+        }
+      }
+    }
+    .contract {
+      font-size: 13px;
+      color: #333333;
+      padding: 25px;
+      p {
+        line-height: 30px;
+      }
+      .retract {
+        margin-left: 25px;
+      }
+      .serial {
+        float: right;
+      }
+      .contract-title {
+        clear: both;
+        font-weight: 700;
+        font-style: normal;
+        font-size: 22px;
+        text-align: center;
+        margin: 20px 0 60px 0;
+      }
+      .identity {
+        display: flex;
+        align-items: center;
+        div {
+          background-color: rgb(230, 247, 255);
+          border: 1px solid rgb(145, 213, 255);
+          font-size: 12px;
+          color: #1890ff;
+          line-height: 20px;
+          width: 100px;
+          text-align: center;
+        }
+      }
+      .focus {
+        font-weight: 700;
+        text-decoration: underline;
+      }
+      .sign {
+        margin-top: 50px;
+        display: flex;
+
+        .party-sign {
+          p {
+            line-height: 35px;
+          }
         }
       }
     }
@@ -738,7 +956,7 @@ export default {
   .obj-btn {
     display: flex;
     padding: 0 45px;
-    margin: 30px 0 50px 0;
+    margin: 30px 0 70px 0;
     .btns-all {
       color: #ffffff;
       font-weight: 700;
@@ -754,8 +972,8 @@ export default {
     .stop-obj {
       background-color: rgb(51, 153, 255);
     }
-    .no-obj{
-      background-color: rgb(174,174,174);
+    .no-obj {
+      background-color: rgb(174, 174, 174);
     }
   }
 }
