13d8497cd8d7d2fc6072812bc8144492799626ee
lilu1233 <lilusiring@163.com>
2019-12-19 11:35:35 +0800

    pull

--- admin/application/api/controller/NeedOrder.php
+++ admin/application/api/controller/NeedOrder.php
@@ -81,7 +81,7 @@ class  NeedOrder  extends  Base
         if ($postData) {
             $need = new Need();
             $param['user_id'] = $postData['user_id'];
-            $data = $need->get_need_order($param);
+            $data = $need->get_need_order($param,1);
             return $data ? returnJson(1, '获取成功', $data) : returnJson(0, '获取失败');
         } else {
             returnJson(0, '获取失败');
