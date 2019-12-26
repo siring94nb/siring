e66258dba4f65858da7cfe3a65c1f3a094d061bc
Zheng-jp <970663468@qq.com>
2019-12-26 10:34:24 +0800

    pull

---
+++ frontend/src/views/alipay.vue
@@ -0,0 +1,26 @@
+<template>
+	<div v-html="apply">
+        {{apply}}
+    </div>
+</template>
+ 
+<script>
+export default {
+    name:"alipay",
+    data(){
+        return {
+            apply:''
+         }
+    },
+   	mounted(){
+         this.apply = this.$route.query.pdf;
+         console.log(this.apply)
+       	 this.$nextTick(()=> {
+               document.forms[0].submit()
+       	 })
+    }
+}
+</script>
+ 
+<style>
+</style>
