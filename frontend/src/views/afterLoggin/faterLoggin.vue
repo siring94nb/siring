1362fd3297e4ea9d3836feef72d3b7b861a72af9
Lan Penmiao <mr_lanpm@163.com>
2020-01-15 14:45:53 +0800

    0115

--- frontend/src/views/afterLoggin/faterLoggin.vue
+++ frontend/src/views/afterLoggin/faterLoggin.vue
@@ -183,6 +183,12 @@ export default {
     // };
   },
   methods: {
+    showMsg(msg, code) {
+      this.$message({
+        message: msg,
+        type: code === 1 ? "success" : "error"
+      });
+    },
     init() {
       console.log("0109-a");
       this.cityHehuorenX(),
@@ -221,12 +227,23 @@ export default {
     // 城市合伙人
     cityHehuorenX() {
       // 因为前期想法错误，当前修改较麻烦
-      GetRoleCenter().then(res => {
+      const params = {
+        type: 1
+      };
+      GetRoleCenter(params).then(res => {
         let { data, msg,code } = res;
         console.log(res);
         if (code == 1) {  
           this.arr[2].con[0].rou = "/CityPartner";
-        } else {
+        }else if(code == 3){
+          this.showMsg(msg,code);
+          this.$router.push({
+            name:`index`,
+            params:{
+              isRegister:'2'
+            }
+          })
+        }else {
           // this.arr[2].con[0].rou = "/CityPartner";
           this.arr[2].con[0].rou = "/partnerCityX";
           // 13260676780
@@ -235,11 +252,22 @@ export default {
     },
     // 等级会员
     classHuiyuanX() {
-      GetRoleCenter().then(res => {
+      const params = {
+        type: 2
+      };
+      GetRoleCenter(params).then(res => {
         let { data, msg,code} = res;
         if (code == 1) {
           this.arr[2].con[1].rou = "/ClassMembersA";
-        } else {
+        } else if(code == 3){
+          // this.showMsg(msg,code);
+          this.$router.push({
+            name:`index`,
+            params:{
+              isRegister:'2'
+            }
+          })
+        }else {
           this.arr[2].con[1].rou = "/ClassMembersX";
           // this.arr[2].con[1].rou = "/ClassMembersA";
           // 13260676780
@@ -248,11 +276,22 @@ export default {
     },
     // 分包商
     fenbaoshangX() {
-      GetRoleCenter().then(res => {
+      const params = {
+        type: 3
+      };
+      GetRoleCenter(params).then(res => {
         let { data, msg, code } = res;
         if (code == 1) {
           this.arr[2].con[2].rou = "/subContractorSm1";
-        } else {
+        } else if(code == 3){
+          // this.showMsg(msg,code);
+          this.$router.push({
+            name:`index`,
+            params:{
+              isRegister:'2'
+            }
+          })
+        }else {
           this.arr[2].con[2].rou = "/subContractorIndex";
           // this.arr[2].con[2].rou = "/subContractorSm1";
           // 13260676780
